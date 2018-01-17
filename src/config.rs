ebba7585c852e6199b0268fa8e281e1f9c36fda2
Fabian Schuiki <fschuiki@iis.ee.thz.ch>
2018-01-17 18:00:37 +0100

    Add dependency field to manifest
    Extend the package manifest to accept dependency specifications as a
    hash map. The manifest itself accepts `PartialDependency` objects, which
    can be validated into an actual `Dependency`. This step parses the
    version requirements and ensures that only legal combinations of the
    fields are used. Validation not yet implemented.

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,9 @@
 
 #![deny(missing_docs)]
 
+use std::collections::HashMap;
+use semver::Version;
+
 /// A package manifest.
 ///
 /// This is usually called `Landa.yml` in the root directory of the package.
@@ -15,6 +18,8 @@
 pub struct Manifest {
     /// The package definition.
     pub package: Option<Package>,
+    /// The dependencies.
+    pub dependencies: HashMap<String, PartialDependency>,
 }
 
 /// A package definition.
@@ -27,4 +32,48 @@ pub struct Package {
     /// A list of package authors. Each author should be of the form `John Doe
     /// <john@doe.com>`.
     pub authors: Option<Vec<String>>,
+}
+
+/// A partial dependency.
+///
+/// Contains all the necessary information to resolve and find a dependency.
+/// The following combinations of fields are valid:
+///
+/// - `version`
+/// - `path`
+/// - `git,rev`
+/// - `git,version`
+///
+/// Can be validated into a `Dependency`.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PartialDependency {
+    /// The path to the package.
+    path: Option<String>,
+    /// The git URL to the package.
+    git: Option<String>,
+    /// The git revision of the package to use. Can be a commit hash, branch,
+    /// tag, or similar.
+    rev: Option<String>,
+    /// The version requirement of the package. This will be parsed into a
+    /// semantic versioning requirement.
+    version: Option<String>,
+}
+
+/// A dependency.
+///
+/// The name of the dependency is given implicitly by the key in the hash map
+/// that this `Dependency` is accessible through.
+pub enum Dependency {
+    /// A dependency that can be found in one of the package repositories.
+    Version(Version),
+    /// A local path dependency. The exact version of the dependency found at
+    /// the given path will be used, regardless of any actual versioning
+    /// constraints.
+    Path(String),
+    /// A git dependency specified by a revision.
+    GitRevision(String, String),
+    /// A git dependency specified by a version requirement. Works similarly to
+    /// the `GitRevision`, but extracts all tags of the form `v.*` from the
+    /// repository and matches the version against that.
+    GitVersion(String, Version),
 }
