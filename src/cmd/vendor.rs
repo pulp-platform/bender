683de39ae3dc6cdd68b8bd074781c89d34fac40f
Michael Rogenmoser <michael@rogenmoser.us>
2022-08-18 14:38:07 +0200

    vendor: Add arguments to clarify action

--- src/cmd/vendor.rs
+++ src/cmd/vendor.rs
@@ -5,7 +5,7 @@
 
 use crate::config::PrefixPaths;
 use crate::futures::TryFutureExt;
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 use futures::future::{self};
 use tokio::runtime::Runtime;
 
@@ -21,10 +21,26 @@ use tempdir::TempDir;
 pub fn new<'a>() -> Command<'a> {
     Command::new("vendor")
         .about("Copy source code from upstream vendor repositories into this repository")
+        .arg(
+            Arg::new("refetch")
+                .long("refetch")
+                .help("Replace the vendored files from upstream and apply the patches"),
+        )
+        .arg(
+            Arg::new("no_patch")
+                .short('n')
+                .long("no_patch")
+                .help("Do not apply patches when refetching dependencies"),
+        )
+        .arg(
+            Arg::new("gen_patch")
+                .long("gen_patch")
+                .help("Generate Patch file from changes to the upstream"),
+        )
 }
 
 /// Execute the `vendor` subcommand.
-pub fn run(sess: &Session, _matches: &ArgMatches) -> Result<()> {
+pub fn run(sess: &Session, matches: &ArgMatches) -> Result<()> {
     let rt = Runtime::new()?;
 
     for vendor_package in &sess.manifest.vendor {
@@ -77,98 +93,101 @@ pub fn run(sess: &Session, _matches: &ArgMatches) -> Result<()> {
             DependencySource::Registry => unimplemented!(),
         };
 
-        // import necessary files from upstream, apply patches
-        stageln!("Copying", "{} files from upstream", vendor_package.name);
-        // Remove existing directories before importing them again
-        std::fs::remove_dir_all(vendor_package.target_dir.clone()).unwrap_or(());
+        if matches.is_present("refetch") {
+            // import necessary files from upstream, apply patches
+            stageln!("Copying", "{} files from upstream", vendor_package.name);
+            // Remove existing directories before importing them again
+            std::fs::remove_dir_all(vendor_package.target_dir.clone()).unwrap_or(());
 
-        vendor_package
-            .mapping
-            .clone()
-            .into_iter()
-            .try_for_each::<_, Result<_>>(|link| {
-                // Make sure the target directory actually exists
-                std::fs::create_dir_all(&link.to.parent().unwrap())?;
+            vendor_package
+                .mapping
+                .clone()
+                .into_iter()
+                .try_for_each::<_, Result<_>>(|link| {
+                    // Make sure the target directory actually exists
+                    std::fs::create_dir_all(&link.to.parent().unwrap())?;
 
-                // Copy src to dst recursively.
-                match &link.from.clone().prefix_paths(&dep_path).is_dir() {
-                    true => copy_recursively(
-                        &link.from.prefix_paths(&dep_path),
-                        &link.to,
-                        &vendor_package
-                            .exclude_from_upstream
-                            .clone()
-                            .into_iter()
-                            .map(|excl| format!("{}/{}", &dep_path.to_str().unwrap(), &excl))
-                            .collect(),
-                    )?,
-                    false => {
-                        std::fs::copy(&link.from.prefix_paths(&dep_path), &link.to)?;
-                    }
-                };
-                Ok(())
-            })?;
-
-        vendor_package
-            .mapping
-            .clone()
-            .into_iter()
-            .try_for_each::<_, Result<_>>(|link| {
-                match link.patch_dir {
-                    Some(patch) => {
-                        // Create directory in case it does not already exist
-                        std::fs::create_dir_all(patch.clone())?;
-
-                        let mut patches = std::fs::read_dir(patch)?
-                            .map(move |f| f.unwrap().path())
-                            .filter(|f| f.extension().unwrap() == "patch")
-                            .collect::<Vec<_>>();
-                        patches.sort_by_key(|patch_path| patch_path.to_str().unwrap().to_lowercase());
-
-                        // for all patches in this directory, git apply them to the to directory
-                        let git = Git::new(sess.root, &sess.config.git);
-                        let to_link = link
-                            .to
-                            .strip_prefix(sess.root)
-                            .map_err(|cause| Error::chain("Failed to strip path.", cause))?;
-                        for patch in patches {
-                            rt.block_on(async {
-                                // TODO MICHAERO: May need throttle
-                                future::lazy(|_| {
-                                    stageln!(
-                                        "Patching",
-                                        "{} with {}",
-                                        vendor_package.name,
-                                        patch.file_name().unwrap().to_str().unwrap()
-                                    );
-                                    Ok(())
-                                })
-                                .and_then(|_| {
-                                    git.spawn_with(|c| {
-                                        c.arg("apply")
-                                            .arg("--directory")
-                                            .arg(&to_link)
-                                            .arg("-p1")
-                                            .arg(&patch)
-                                    })
-                                })
-                                .await
-                                .map_err(move |cause| {
-                                    Error::chain(
-                                        format!("Failed to apply patch {:?}.", patch),
-                                        cause,
-                                    )
-                                })
-                                .map(move |_| git)
-                            })?;
+                    // Copy src to dst recursively.
+                    match &link.from.clone().prefix_paths(&dep_path).is_dir() {
+                        true => copy_recursively(
+                            &link.from.prefix_paths(&dep_path),
+                            &link.to,
+                            &vendor_package
+                                .exclude_from_upstream
+                                .clone()
+                                .into_iter()
+                                .map(|excl| format!("{}/{}", &dep_path.to_str().unwrap(), &excl))
+                                .collect(),
+                        )?,
+                        false => {
+                            std::fs::copy(&link.from.prefix_paths(&dep_path), &link.to)?;
                         }
-                    }
-                    None => {}
-                };
-                Ok(())
-            })?;
+                    };
+                    Ok(())
+                })?;
 
-        // TODO: Update lockfile
+            if !matches.is_present("no_patch") {
+                vendor_package
+                    .mapping
+                    .clone()
+                    .into_iter()
+                    .try_for_each::<_, Result<_>>(|link| {
+                        match link.patch_dir {
+                            Some(patch) => {
+                                // Create directory in case it does not already exist
+                                std::fs::create_dir_all(patch.clone())?;
+
+                                let mut patches = std::fs::read_dir(patch)?
+                                    .map(move |f| f.unwrap().path())
+                                    .filter(|f| f.extension().unwrap() == "patch")
+                                    .collect::<Vec<_>>();
+                                patches.sort_by_key(|patch_path| {
+                                    patch_path.to_str().unwrap().to_lowercase()
+                                });
+
+                                // for all patches in this directory, git apply them to the to directory
+                                let git = Git::new(sess.root, &sess.config.git);
+                                let to_link = link.to.strip_prefix(sess.root).map_err(|cause| {
+                                    Error::chain("Failed to strip path.", cause)
+                                })?;
+                                for patch in patches {
+                                    rt.block_on(async {
+                                        // TODO MICHAERO: May need throttle
+                                        future::lazy(|_| {
+                                            stageln!(
+                                                "Patching",
+                                                "{} with {}",
+                                                vendor_package.name,
+                                                patch.file_name().unwrap().to_str().unwrap()
+                                            );
+                                            Ok(())
+                                        })
+                                        .and_then(|_| {
+                                            git.spawn_with(|c| {
+                                                c.arg("apply")
+                                                    .arg("--directory")
+                                                    .arg(&to_link)
+                                                    .arg("-p1")
+                                                    .arg(&patch)
+                                            })
+                                        })
+                                        .await
+                                        .map_err(move |cause| {
+                                            Error::chain(
+                                                format!("Failed to apply patch {:?}.", patch),
+                                                cause,
+                                            )
+                                        })
+                                        .map(move |_| git)
+                                    })?;
+                                }
+                            }
+                            None => {}
+                        };
+                        Ok(())
+                    })?;
+            }
+        }
     }
 
     Ok(())
