52e4cb354ae853cddb0375c171dc52e2a3609e5d
Michael Rogenmoser <michael@rogenmoser.us>
2022-09-13 09:56:37 +0200

    Add almalinux CI packages

--- .github/scripts/gen_dockerfile.sh
+++ .github/scripts/gen_dockerfile.sh
@@ -18,17 +18,19 @@ fi
 if [ $(echo $full_tgtname | cut -d ':' -f 1) = "debian" ]; then
   echo 'RUN apt update && apt -y install build-essential curl gcc make' >> $filename
 fi
-if [[ $(echo $full_tgtname | cut -d ':' -f 1) == "redhat"* ]]; then
-  echo 'RUN yum install cargo -y' >> $filename
-else
-  echo >> $filename
-  echo 'ENV RUSTUP_HOME=/usr/local/rustup CARGO_HOME=/usr/local/cargo' >> $filename
-  echo 'ENV PATH $CARGO_HOME/bin:$PATH' >> $filename
-  echo >> $filename
-  echo 'RUN mkdir -p "$CARGO_HOME" && mkdir -p "$RUSTUP_HOME" && \' >> $filename
-  echo '    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable && \' >> $filename
-  echo '    chmod -R a=rwX $CARGO_HOME' >> $filename
-  echo >> $filename
+if [ $(echo $full_tgtname | cut -d ':' -f 1) = "almalinux" ]; then
+  echo 'RUN dnf -y update && dnf -y group install "Development Tools"'
 fi
+if [[ $(echo $full_tgtname | cut -d ':' -f 1) == "redhat"* ]]; then
+  echo 'RUN dnf -y install gcc'
+fi
+echo >> $filename
+echo 'ENV RUSTUP_HOME=/usr/local/rustup CARGO_HOME=/usr/local/cargo' >> $filename
+echo 'ENV PATH $CARGO_HOME/bin:$PATH' >> $filename
+echo >> $filename
+echo 'RUN mkdir -p "$CARGO_HOME" && mkdir -p "$RUSTUP_HOME" && \' >> $filename
+echo '    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable && \' >> $filename
+echo '    chmod -R a=rwX $CARGO_HOME' >> $filename
+echo >> $filename
 echo 'WORKDIR /source' >> $filename
 
