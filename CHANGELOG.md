190319a147547af2de77c13e4656934a1fe18bf4
Michael Rogenmoser <michael@rogenmoser.us>
2023-09-07 16:14:15 +0200

    Add `--no-checkout` flag to `path` command to avoid checkout if not needed.
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- Add `--no-checkout` flag to `path` command to avoid checkout if not needed.
+
 ### Fixed
 - Improve ReadMe and Warning information for `vendor` upstream linking.
 - Ensure `workspace.package_links` symlinks are properly updated when executing the `clone` command.
--- src/cmd/path.rs
+++ src/cmd/path.rs
@@ -3,7 +3,7 @@
 
 //! The `path` subcommand.
 
-use clap::{Arg, ArgMatches, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 use futures::future::join_all;
 use tokio::runtime::Runtime;
 
@@ -20,31 +20,50 @@ pub fn new() -> Command {
                 .required(true)
                 .help("Package names to get the path for"),
         )
+        .arg(
+            Arg::new("no-checkout")
+                .long("no-checkout")
+                .num_args(0)
+                .action(ArgAction::SetTrue)
+                .help("Prevents check out of dependency."),
+        )
 }
 
 /// Execute the `path` subcommand.
 pub fn run(sess: &Session, matches: &ArgMatches) -> Result<()> {
-    let rt = Runtime::new()?;
-    let io = SessionIo::new(sess);
-
     let ids = matches
         .get_many::<String>("name")
         .unwrap()
         .map(|n| Ok((n, sess.dependency_with_name(&n.to_lowercase())?)))
         .collect::<Result<Vec<_>>>()?;
-    debugln!("main: obtain checkouts {:?}", ids);
-    let checkouts = rt
-        .block_on(join_all(
-            ids.iter()
-                .map(|&(_, id)| io.checkout(id))
-                .collect::<Vec<_>>(),
-        ))
-        .into_iter()
-        .collect::<Result<Vec<_>>>()?;
-    debugln!("main: checkouts {:#?}", checkouts);
-    for c in checkouts {
-        if let Some(s) = c.to_str() {
-            println!("{}", s);
+
+    let io = SessionIo::new(sess);
+    if !matches.get_flag("no-checkout") {
+        debugln!("main: obtain checkouts {:?}", ids);
+        let rt = Runtime::new()?;
+        let checkouts = rt
+            .block_on(join_all(
+                ids.iter()
+                    .map(|&(_, id)| io.checkout(id))
+                    .collect::<Vec<_>>(),
+            ))
+            .into_iter()
+            .collect::<Result<Vec<_>>>()?;
+        debugln!("main: checkouts {:#?}", checkouts);
+        for c in checkouts {
+            if let Some(s) = c.to_str() {
+                println!("{}", s);
+            }
+        }
+    } else {
+        let paths = ids
+            .iter()
+            .map(|&(_, id)| io.get_package_path(id))
+            .collect::<Vec<_>>();
+        for c in paths {
+            if let Some(s) = c.to_str() {
+                println!("{}", s);
+            }
         }
     }
     Ok(())
