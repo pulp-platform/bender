2b278e2ae52eefed7ad76b903661f179830f5234
Philip Wiese <wiesep@iis.ee.ethz.ch>
2025-05-20 17:09:20 +0200

    Implement PR feedback

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script expects two optional arguments:
+# 1. The target architecture (e.g., x86_64, aarch64)
+# 2. The target OS (e.g., linux, windows, macos)
+
 if [[ "$GITHUB_REF" =~ ^refs/tags/v.*$ ]]; then
     pkgver="$(echo $GITHUB_REF | sed -n 's/^refs\/tags\/v//p')"
 else
@@ -10,18 +14,18 @@ if [ -z "$pkgver" ]; then
     pkgver="latest"
 fi
 
-if [ -z "$1" ] && [ -z "$2" ]; then # no arguments
+if [ -z "$2" ] && [ -z "$1" ]; then # no arguments
     release_dir="target/release"
     tar_suffix=""
     tar_prefix="-x86_64"
-elif [ -n "$1" ] && [ -n "$2" ]; then # both arguments
-    release_dir="target/$2/$1/release"
-    tar_suffix="-$1"
-    tar_prefix="-$2"
 elif [ -n "$1" ] && [ -z "$2" ]; then # only first argument
     release_dir="target/$1/release"
-    tar_suffix="-$1"
-    tar_prefix="-x86_64"
+    tar_suffix=""
+    tar_prefix="-$1"
+elif [ -n "$2" ] && [ -n "$1" ]; then # both arguments
+    release_dir="target/$1/$2/release"
+    tar_suffix="-$2"
+    tar_prefix="-$1"
 fi
 
 # WIESEP: Change amd64 to x86_64 to keep release names compatible with previous releases
