751c8fdf3ae1ca986d968c0641725a30780e8471
Philip Wiese <wiesep@iis.ee.ethz.ch>
2025-05-20 11:04:46 +0200

    Multi-Arch Build

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -6,15 +6,25 @@ else
     readonly pkgver="$(echo $GITHUB_REF | sed -n 's/^refs\/tags\///p')"
 fi
 
-if [ -z "$1" ]; then
-    readonly release_dir="target/release"
-    readonly tar_suffix=""
-else
-    readonly release_dir="target/$1/release"
-    readonly tar_suffix="-$1"
+if [ -n "$pkgver" ]; then
+    pkgver="-$pkgver"
 fi
 
-tar -czf "bender-$pkgver-x86_64-linux-gnu$tar_suffix.tar.gz" \
+if [ -z "$1" ] && [ -z "$2" ]; then # no arguments
+    readonly release_dir="target/release"
+    readonly tar_suffix=""
+    readonly tar_prefix=""
+elif [ -n "$1" ] && [ -n "$2" ]; then # both arguments
+    readonly release_dir="target/$2/$1/release"
+    readonly tar_suffix="-$1"
+    readonly tar_prefix="-$2"
+elif [ -n "$1" ] && [ -z "$2" ]; then # only first argument
+    readonly release_dir="target/$1/release"
+    readonly tar_suffix="-$1"
+    readonly tar_prefix=""
+fi
+
+tar -czf "bender$pkgver$tar_prefix-linux-gnu$tar_suffix.tar.gz" \
         -C "./$release_dir" \
         --owner=0 --group=0 \
         bender
